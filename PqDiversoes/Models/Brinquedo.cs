ab0b32c2eb541078b24f5f14f611a513ba5b63ae
dclaumanndeveloper <contatodclaumann@gmail.com>
2017-04-14 16:42:40 -0300

    adicao classe brinquedo

---
+++ PqDiversoes/Models/Brinquedo.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace PqDiversoes.Models
+{
+    public class Brinquedo
+    {
+        public int ID{get;set;}
+        public String Nome{get;set;}
+        public DateTime Manutencao{get;set;}
+    }
+}
