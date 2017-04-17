1d3881bc97b45f04eb9d622c6bbe8d99046a10fb
Dclaumann <contgatodclaumann@gmail.com>
2017-04-17 08:15:16 -0300

    alteração classe brinquedo

--- PqDiversoes/Models/Brinquedo.cs
+++ PqDiversoes/Models/Brinquedo.cs
@@ -1,13 +1,23 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
 
 
 namespace PqDiversoes.Models
 {
     public class Brinquedo
     {
+        [Key]
         public int ID{get;set;}
+        [Display(Name="Brinquedo")]
         public String Nome{get;set;}
+        [Display(Name="Manutenção")]
         public DateTime Manutencao{get;set;}
+        [Display(Name="Ultima Manutenção")]
         public DateTime UltimaManutencao{get;}
     }
 }
