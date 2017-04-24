68beb2d02f9dbb09b79a2931e8819f5000eedb81
Aluno <Aluno@050LAB000X>
2017-04-24 08:53:35 -0300

    Alteração das mensagens

--- PqDiversoes/Models/ManageViewModels.cs
+++ PqDiversoes/Models/ManageViewModels.cs
@@ -28,14 +28,14 @@ namespace PqDiversoes.Models
     public class SetPasswordViewModel
     {
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "A {0} deve ter no minimo {2} caracteres.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Nova Senha")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display(Name = "Confirmar Nova Senha")]
+        [Compare("NewPassword", ErrorMessage = "As senhas não combinam.")]
         public string ConfirmPassword { get; set; }
     }
 
@@ -47,9 +47,9 @@ namespace PqDiversoes.Models
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "A {0} deve ter no minimo {2} caracteres.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Nova Senha")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
